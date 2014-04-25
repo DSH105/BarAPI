9096fa419bb8840ddb95a97b1d81756c0bdd2a24
DSH105 <jason.chirgwin@icloud.com>
2014-04-25 11:55:20 +1000

    I have no idea

--- src/main/java/com/dsh105/barapi/reflection/utility/RemappedClassHandler.java
+++ src/main/java/com/dsh105/barapi/reflection/utility/RemappedClassHandler.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of BarAPI (1) (me.confuser.barapi).
- *
- * BarAPI (1) (me.confuser.barapi) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BarAPI (1) (me.confuser.barapi) is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with BarAPI (1) (me.confuser.barapi).  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /**
  * Used to provide functionality with MCPC+, as MCPC+ remaps the classes in order to
  * let Mods and Plugins work together in harmony. This is however a downside for Plugins
